37ddfb6a5dfde4748a2d0eb51873a686d66de533
aman2712 <khanamankhan1973@gmail.com>
2025-03-12 01:14:35 +0530

    minor fix

--- app/mobile/page.js
+++ app/mobile/page.js
@@ -11,7 +11,7 @@ export default function MobilePage() {
                     <Image src='/images/logo.png' width={50} height={50} alt="ZennSpace | Logo" />
                     <p className='text-2xl font-bauhaus mt-3 tracking-[10px] ml-4 font-semibold'>ZENNSPACE</p>
                 </div>
-                <p className='pt-10'>We're starting with a desktop experience first.<br />Stay tuned - mobile support is on the way! 🚀</p>
+                <p className='pt-10'>We&apos;re starting with a desktop experience first.<br />Stay tuned - mobile support is on the way! 🚀</p>
                 <p className='pt-20'>Sign up for our newsletter and get exclusive updates straight to your inbox!</p>
                 <EmailSubscription noLabel={true} />
             </div>
--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
     <div className='px-20 py-10 min-h-screen h-auto bg-gray-800 home_page'>
       <Header />
       <div className='mt-14 max-w-1/2'>
-        <p className='text-lg tracking-wide'>We're almost there</p>
+        <p className='text-lg tracking-wide'>We&apos;re almost there</p>
         <h1 className='text-[10rem] leading-40 -ml-4 mt-2 tracking font-semibold'>Coming Soon</h1>
         <p className=' max-w-4/5 mt-10'>Join our newsletter to be the first to know when ZennSpace launches and recieve updates on the latest features and tips for productivity!</p>
 
